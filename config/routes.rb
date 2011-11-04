d8b561c1ec8f325d636796d00969291903a5b5e5
Timothy McKenna <mckenna.tim@gmail.com>
2011-11-03 20:20:57 -0400

    back to

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Ror3::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
